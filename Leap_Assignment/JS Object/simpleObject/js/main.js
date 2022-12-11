de2ff34f87f2a4851099133f6f992bd7fb583008
Joyride77 <nfive8@gmail.com>
2022-12-11 23:13:03 +0800

    assignment add

---
+++ Leap_Assignment/JS Object/simpleObject/js/main.js
@@ -0,0 +1,13 @@
+console.log("Simple object");
+
+let dog = {
+    color : 'black',
+    rName : 'Max',
+    age : 5,
+    personality : 'angry'
+
+
+}
+
+console.log(dog);
+console.log(Object.values(dog));
